2dcb55ceae37f402da385bdc54602686061f50cd
唐书 <leping.wlp@alibaba-inc.com>
2019-01-18 20:38:44 +0800

    gitpages

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# gen-sw-url
-提取网页中需要precache的url信息
---
+++ precache-manifest.03259a6da6a2739110090880484b9592.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a7ec19615f91ea72d878",
+    "url": "./static/js/main.a7ec1961.chunk.js"
+  },
+  {
+    "revision": "1c60245d76ed0f461c06",
+    "url": "./static/js/1.1c60245d.chunk.js"
+  },
+  {
+    "revision": "a7ec19615f91ea72d878",
+    "url": "./static/css/main.00689282.chunk.css"
+  },
+  {
+    "revision": "e2782504583e38f6f9ef83b272127dce",
+    "url": "./index.html"
+  }
+];
